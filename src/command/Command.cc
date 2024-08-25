16bdcb275d3c050ae624b76ac3fc29b95ec3835f
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2024-08-25 16:43:56 +0800

    chore: test Unbreakable

--- src/command/Command.cc
+++ src/command/Command.cc
@@ -5,8 +5,13 @@
 #include "ll/api/form/SimpleForm.h"
 #include "magic_enum.hpp"
 #include "mc/world/level/block/Block.h"
-using ConfImpl = fm::config::ConfImpl;
 
+#include "mc/nbt/CompoundTag.h"
+#include "mc/world/item/Item.h"
+#include "mc/world/item/registry/ItemStack.h"
+
+
+using ConfImpl = fm::config::ConfImpl;
 
 namespace fm::command {
 
@@ -51,6 +56,34 @@ void registerCommand() {
         }
         gui::sendBlockManager(pl);
     });
+
+#ifdef DEBUG
+    // fm debug unbreakable
+    cmd.overload().text("debug").text("unbreakable").execute([](CommandOrigin const& ori, CommandOutput& out) {
+        if (ori.getOriginType() != CommandOriginType::Player) {
+            return utils::sendText<utils::Level::Error>(out, "此命令仅限玩家使用");
+        }
+        Player& pl = *static_cast<Player*>(ori.getEntity());
+
+        auto& item = const_cast<ItemStack&>(pl.getSelectedItem());
+        if (item.isNull()) {
+            return utils::sendText<utils::Level::Error>(out, "请先选择一个物品");
+        }
+
+        auto nbt = item.save();
+        auto tag = nbt->getCompound("tag");
+        if (!tag) {
+            nbt->putCompound("tag", CompoundTag{});
+        }
+        nbt->getCompound("tag")->putByte("Unbreakable", (byte) true);
+
+        item.load(*nbt);
+        pl.refreshInventory();
+
+        utils::sendText(pl, "设置已保存");
+    });
+
+#endif
 }
 
 
